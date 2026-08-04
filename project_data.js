f2e8e37183229f5a537b6e3345ad2a3d53b81b26
Michael Navarro <m.navarro@gmx.net>
2026-08-04 19:08:31 +0200

    feat: set up js logic for popup window

---
+++ project_data.js
@@ -0,0 +1,18 @@
+const projects = [
+    {
+        id = "join",
+        project_nr: "01",
+        title: "Join",
+        description: "A task manager inspired by the Kanban system: create and organize tasks using drag-and-drop functionality, assign users, and categorize tasks.",
+        image: "./assets/img/join_.svg",
+        tech: ['./assets/icons/html_.svg', './assets/icons/css_.svg', './assets/icons/Javascript_.svg', './assets/icons/firebase_.svg'],
+    },
+    {
+        id = "pollo-loco",
+        project_nr: "02",
+        title: "Pollo Loco",
+        description: "A task manager inspired by the Kanban system: create and organize tasks using drag-and-drop functionality, assign users, and categorize tasks.",
+        image: "./assets/img/polloloco.svg",
+        tech: ['./assets/icons/html_.svg', './assets/icons/css_.svg', './assets/icons/Javascript_.svg'],
+    }
+];
--- script.js
+++ script.js
@@ -24,3 +24,49 @@ function getProjectImage(projectName) {
         `.project-image[data-project="${projectName}"]`
     );
 }
+
+function openProjectPopup(project) {
+    let proj = 'join'
+
+    if (project == proj) {
+        console.log('popup opened for join')
+    } else (
+        console.log('popup opened for pollo loco')
+    );
+
+}
+
+function renderPopupTemplate() {
+    let content = document.getElementById('popup-content');
+    content.innerHTML = '';
+    for (let i = 0; i < projects.length; i++) {
+        
+    }
+    return /*html*/ `
+        <div class="popup-wrapper">
+            <div class="popup-left-container">
+                <h2></h2>
+                <p></p>
+                <p>What is this project about?</p>
+                <p class="description"></p>
+                <div class="tech-icons">
+
+                </div>
+                <div class="popup-button">
+                    <button>GitHub</button>
+                    <button>LiveTest</button>
+                </div>
+                
+            </div>
+
+            <div class="popup-right-container">
+                <img src="./assets/icons/close_small.svg" alt="close icon">
+                <img src="" alt="">
+                <div>
+                    <p>Next project</p>
+                </div>
+            </div>
+
+        </div>
+    `
+}
